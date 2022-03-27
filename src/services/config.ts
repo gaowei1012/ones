8f05c5be064667c99ff9d1b4c34b6985248f77d2
gaowei1012 <gyb2020018@163.com>
2022-03-27 22:37:11 +0800

    add 添加API路由配置

--- src/services/config.ts
+++ src/services/config.ts
@@ -1,6 +1,20 @@
-import config from '@/config/const'
+/*
+ * @Author: mingwei
+ * @Date: 2022-03-21 09:17:26
+ * @LastEditors: mingwei
+ * @LastEditTime: 2022-03-27 22:35:34
+ * @FilePath: /ones/src/services/config.ts
+ * @Description: API路由配置
+ */
+import Config from 'react-native-config'
 let base_domain: any
 
-base_domain = 'http://' + config.dataServerDev.host
+if (Config.MODE == 'development') {
+  base_domain = 'http://' + Config.BASE_URL + ':' + Config.PORT
+} else if (Config.MODE == 'production') {
+  base_domain = 'http://' + Config.BASE_URL + ':' + Config.PORT
+} else if (Config.MODE == 'test') {
+  base_domain = 'http://' + Config.BASE_URL
+}
 
-export const BASE_PATH = base_domain + ':' + config.dataServerDev.port
+export const BASE_PATH = base_domain
